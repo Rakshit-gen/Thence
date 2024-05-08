a5679902be161d3a99c651820ee1670a788343f6
Saransh Bangar <saranshbangad@gmail.com>
2024-05-08 10:42:56 +0530

    improved button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,11 @@ const Button = ({ backgroundColor, borderColor, textColor, hoverColor, label }) 
 
     const buttonStyle = {
         backgroundColor: isHovered ? hoverColor : backgroundColor,
-        border: borderColor,
+        border: `1px solid ${borderColor}`,
         color: textColor,
         padding: '1rem 1.75rem',
         fontSize: '1.125rem',
-        border: '1px solid',
+        // border: '1px solid',
         fontFamily: 'Manrope',
         borderRadius: '9999px',
         transition: 'background-color 0.2s ease',
