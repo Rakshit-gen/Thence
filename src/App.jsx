434a29ffb0bdef2f3984e93b371bab3975db5074
Saransh Bangar <saranshbangad@gmail.com>
2024-05-08 08:30:21 +0530

    Set up page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,41 @@ import FAQ from "./components/FAQ"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Register from "./pages/register/Register"
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom"
+import { useContext } from "react"
 
 function App() {
+
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Hero />
+        <FAQ />
+        <Footer />
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Layout />
+      )
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <FAQ />
-      <Footer />
-    </>
-  )
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
